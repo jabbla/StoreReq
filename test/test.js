580a318af43f166786bcf6bf803e1f1fb6d0d128
xiaoran.zhu <gloria_n@yeah.net>
2017-02-26 13:15:55 +0800

    initial version
    不完全版本

---
+++ test/test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let ent = require('../lib/bundle.js');
+let Store = ent.Store,
+	Request = ent.Request;
+
+let testReq = new Request({
+	url: 'https://www.baidu.com/'
+});
+
+testReq.request().then(htmlText=>{
+	console.log(htmlText);
+}).catch((error)=>{
+	console.log(error);
+})
